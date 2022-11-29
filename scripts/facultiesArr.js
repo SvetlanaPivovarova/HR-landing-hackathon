b58b1a38f2d46d4400981acb5cc6ca39724bd2d0
Djamal Fataliev <jonipux@yandex.ru>
2022-11-29 14:10:27 +0300

    Final section faculties

---
+++ scripts/facultiesArr.js
@@ -0,0 +1,38 @@
+export const faculties = [
+  {
+    title: 'Программирование',
+    img: '/HR-landing-hackathon/images/demo-img.jpg',
+    courses: 'Тестирование, фронтенд, бэкенд, DevOps и алгоритмы',
+    quantity: '31 КУРС'
+  },
+  {
+    title: 'Анализ данных',
+    img: '/HR-landing-hackathon/images/demo-img.jpg',
+    courses: 'SQL, аналитика, Data Science и архитектура данных',
+    quantity: '16 КУРСОВ'
+  },
+  {
+    title: 'Дизайн',
+    img: '/HR-landing-hackathon/images/demo-img.jpg',
+    courses: 'Графический, коммуникационный, для интерфейсов и продуктов',
+    quantity: '11 КУРСОВ'
+  },
+  {
+    title: 'Маркетинг',
+    img: '/HR-landing-hackathon/images/demo-img.jpg',
+    courses: 'Интерснет-маркетинг и управление трафиком',
+    quantity: '2 КУРСА'
+  },
+  {
+    title: 'Менеджмент',
+    img: '/HR-landing-hackathon/images/demo-img.jpg',
+    courses: 'Управление проектами, командами и IT-рекрутмент',
+    quantity: '12 КУРСОВ'
+  },
+  {
+    title: 'Английский',
+    img: '/HR-landing-hackathon/images/demo-img.jpg',
+    courses: 'Для работы и жизни',
+    quantity: '4 КУРСА'
+  }
+];
