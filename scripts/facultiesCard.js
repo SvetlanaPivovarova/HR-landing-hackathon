23b20a66e0bb29a6ba78cda5a65667a059bed809
Djamal Fataliev <jonipux@yandex.ru>
2022-11-29 14:28:01 +0300

    Faculties ui-update

--- scripts/facultiesCard.js
+++ scripts/facultiesCard.js
@@ -1,7 +1,6 @@
 export class facultiesCard {
   constructor(element) {
     this._title = element.title;
-    this._img = element.img;
     this._courses = element.courses;
     this._quantity = element.quantity;
   }
@@ -14,26 +13,10 @@ export class facultiesCard {
   generateCard() {
     this._element = this._getTemplate();
 
-    this._setEventListeners();
-
-    this._facultiesTextContainer = this._element.querySelector('.faculties-card__text-container');
-    this._facultiesImg = this._element.querySelector('.faculties-card__img');
-
     this._element.querySelector('.faculties-card__title').textContent = this._title;
-    this._facultiesImg.src = this._img;
-    this._facultiesImg.alt = this._title;
     this._element.querySelector('.faculties-card__courses').textContent = this._courses;
     this._element.querySelector('.faculties-card__number-courses').textContent = this._quantity;
 
     return this._element;
-  }
-  _setEventListeners() {
-    this._element.addEventListener('click', () => {
-      this._showText();
-    });
-  }
-  _showText() {
-    this._facultiesTextContainer.classList.add('faculties-card__text-container_visible');
-    this._facultiesImg.remove();
   }
 }
