7af3f5717d2172b1cf1548e0e3cd0da6456450ef
Djamal Fataliev <jonipux@yandex.ru>
2022-11-30 22:41:16 +0300

    active btn

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,29 @@ const offerBtn = document.querySelector('.offers__btn');
 const popup = document.querySelector('.popup');
 const popupCloseBtn = popup.querySelector('.popup__close');
 
-offerBtn.addEventListener('click', () => {
-  popup.classList.add('popup_active');
-});
+const expertBtn = document.querySelector('.expert__btn');
+const headerBtn = document.querySelector('.header__btn');
+const introBtn = document.querySelector('.intro__button');
+const elementScroll = document.querySelector('#open-offers');
 
+const scroll = element => {
+  element.addEventListener('click', () => {
+    elementScroll.scrollIntoView({behavior: 'smooth'});
+  });
+}
+scroll(expertBtn);
+scroll(headerBtn);
+scroll(introBtn);
+
+const openPopup = element => {
+  element.classList.add('popup_active');
+}
 const closePopup = element => {
   element.classList.remove('popup_active');
 }
+offerBtn.addEventListener('click', () => {
+  openPopup(popup);
+});
 
 popup.addEventListener('click', evt => {
   if(evt.target === popup) {
