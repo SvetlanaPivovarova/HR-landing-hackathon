7a5805970360ec54eada15ff44a88bb76c65642a
svetlana <pivovarova.svetlana@gmail.com>
2022-11-30 08:38:49 +0400

    feat: стили блока роли

--- scripts/roleCard.js
+++ scripts/roleCard.js
@@ -1,14 +1,14 @@
 class roleCard {
   constructor(data) {
-    this._image = data.image;
-    this._heading = data._heading;
+    this._image = data.img;
+    this._heading = data.heading;
     this._text = data.description;
   }
 
   _getTemplate() {
     const role = document
       .querySelector(".role-template")
-      .content.querySelector(".role__card")
+      .content.querySelector(".roles__item")
       .cloneNode(true);
 
     // вернуть DOM-элемент карточки
@@ -18,9 +18,9 @@ class roleCard {
     this._element = this._getTemplate();
 
     // добавить данные
-    this._element.querySelector(".role__image").textContent = this._image;
-    this._element.querySelector(".role__heading").textContent = this._heading;
-    this._element.querySelector(".role__text").textContent = this._text;
+    this._element.querySelector(".roles__image").src = this._image;
+    this._element.querySelector(".roles__heading").textContent = this._heading;
+    this._element.querySelector(".roles__text").textContent = this._text;
 
     // вернуть элемент наружу
     return this._element;
