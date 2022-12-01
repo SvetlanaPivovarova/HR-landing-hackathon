4dc34ade1884c4d812eb4077b166c506c2a2c8ba
svetlana <pivovarova.svetlana@gmail.com>
2022-12-01 16:51:34 +0400

    fix: кнопки квиза

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,44 @@
-const offerBtn = document.querySelector('.offers__btn');
-const popup = document.querySelector('.popup');
-const popupCloseBtn = popup.querySelector('.popup__close');
+const offerBtn = document.querySelector(".offers__btn");
+const popup = document.querySelector(".popup");
+const popupCloseBtn = popup.querySelector(".popup__close");
 
-const expertBtn = document.querySelector('.expert__btn');
-const headerBtn = document.querySelector('.header__btn');
-const introBtn = document.querySelector('.intro__button');
-const elementScroll = document.querySelector('#open-offers');
+const expertBtn = document.querySelector(".expert__btn");
+const headerBtn = document.querySelector(".header__btn");
+const introBtn = document.querySelector(".intro__button");
+const elementScroll = document.querySelector("#open-offers");
+const roleButton = document.querySelectorAll(".quiz__button_type_yellow");
 
-const scroll = element => {
-  element.addEventListener('click', () => {
-    elementScroll.scrollIntoView({behavior: 'smooth'});
+const scroll = (element) => {
+  element.addEventListener("click", () => {
+    elementScroll.scrollIntoView({ behavior: "smooth" });
   });
-}
+};
 scroll(expertBtn);
 scroll(headerBtn);
 scroll(introBtn);
+scroll(roleButton[0]);
+scroll(roleButton[1]);
 
-const openPopup = element => {
-  element.classList.add('popup_active');
-}
-const closePopup = element => {
-  element.classList.remove('popup_active');
-}
-offerBtn.addEventListener('click', () => {
+const openPopup = (element) => {
+  element.classList.add("popup_active");
+};
+const closePopup = (element) => {
+  element.classList.remove("popup_active");
+};
+offerBtn.addEventListener("click", () => {
   openPopup(popup);
 });
 
-popup.addEventListener('click', evt => {
-  if(evt.target === popup) {
+popup.addEventListener("click", (evt) => {
+  if (evt.target === popup) {
     closePopup(popup);
   }
 });
-popupCloseBtn.addEventListener('click', () => {
+popupCloseBtn.addEventListener("click", () => {
   closePopup(popup);
 });
-popup.addEventListener('keydown', evt => {
-  if(evt.key === 'Escape') {
+popup.addEventListener("keydown", (evt) => {
+  if (evt.key === "Escape") {
     closePopup(popup);
   }
 });
