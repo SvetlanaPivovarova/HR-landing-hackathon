fdb239cb700da2e24449da1873f0e074bf707aa4
Anna Panteleeva <apanteleeva94@gmail.com>
2022-12-03 18:33:18 +0300

    empty

--- scripts/slider.js
+++
@@ -1,8 +0,0 @@
-const swiper = new Swiper(".swiper", {
-  direction: 'horizontal',
-  loop: true,
-  navigation: {
-    nextEl: ".histories__arrow-next",
-    prevEl: ".histories__arrow-prev",
-  },
-});
